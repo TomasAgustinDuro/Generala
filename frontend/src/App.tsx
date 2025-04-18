ffdc8c94ff9434a58361b80b18c03113857eb491
Tomas <durotomaas@gmail.com>
2025-04-17 23:29:21 -0300

    change details

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css'
-
+import { StrictMode } from 'react'
 import Prueba from './pages/prueba'
 
 function App() {
 
 
   return (
-    <>
+    <StrictMode>
      <Prueba/>
-    </>
+    </StrictMode>
   )
 }
 
